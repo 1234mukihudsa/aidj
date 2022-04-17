0e9a79011cc9af471760d9316fd16274c150685b
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2022-04-17 11:31:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-song1="";
-song2="";
+song="";
+scoreleftWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
@@ -12,13 +12,27 @@ function setup (){
     poseNet =ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotposes);
 }
-function draw(){
-    image(video,0,0,600,500);
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+    fill("#FF0000");
+	stroke("#FF0000");
+	circle(rightWristX,rightWristY,20);
+	
+   if(scoreleftWrist >0.2){
+    InNumberleftWristY = Number(leftWristY);
+	remove_decimals = floor(InNumberleftWristY);
+	volume = remove_decimals/500;
+	document.getElementById("volume").innerHTML = "Volume = " + volume;	
+    song.setVolume(volume);
+   }
 }
 function modelLoaded(){
     console.log('PoseNet Is Intiallized');
 }
 function preload(){
+
+song = loadSound("music.mp3");
 }
 function play(){
     song.play();
@@ -28,6 +42,8 @@ function play(){
 function gotposes(results){
 if(results.length > 0){
     console.log(results);
+    scoreleftWrist=results[0].pose.keypoints[9].score;
+    console.log("scoreLeftWrist =  "+ scoreleftWrist);
     leftWristX=results[0].pose.leftWrist.x;
     leftWristY=results[0].pose.leftWrist.y;
     console.log("leftWristX = " + leftWristX + " leftWristY = "+ leftWristY);
